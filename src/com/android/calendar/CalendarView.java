147c166c8fe1614c160ee71ad868b05a6951457f
Shachar Shemesh <lingnu@gmail.com>
2009-11-21 23:37:21 +0200

    Display the daily appointments via a StaticLayout: 1. Much less code 2. Now supports BiDi

--- src/com/android/calendar/CalendarView.java
+++ src/com/android/calendar/CalendarView.java
@@ -46,6 +46,10 @@ import android.text.format.DateFormat;
 import android.text.format.DateUtils;
 import android.text.format.Time;
 import android.text.TextUtils;
+import android.text.StaticLayout;
+import android.text.Layout.Alignment;
+import android.text.TextPaint;
+import android.text.TextUtils.TruncateAt;
 import android.util.Log;
 import android.view.ContextMenu;
 import android.view.Gravity;
@@ -2197,14 +2201,11 @@ public class CalendarView extends View
             return;
         }
 
-        float width = rf.right - rf.left;
-        float height = rf.bottom - rf.top;
-
         // Leave one pixel extra space between lines
         int lineHeight = mEventTextHeight + 1;
 
         // If the rectangle is too small for text, then return
-        if (width < MIN_CELL_WIDTH_FOR_TEXT || height <= lineHeight) {
+        if (rf.width() < MIN_CELL_WIDTH_FOR_TEXT || rf.height() <= lineHeight) {
             return;
         }
 
@@ -2213,73 +2214,17 @@ public class CalendarView extends View
 
         text = drawTextSanitizer(text);
 
-        int len = text.length();
-        if (len > MAX_EVENT_TEXT_LEN) {
-            text = text.substring(0, MAX_EVENT_TEXT_LEN);
-            len = MAX_EVENT_TEXT_LEN;
-        }
+	// Use a StaticLayout to format the string.
+	canvas.save();
+	canvas.translate( rf.left+1, rf.top ); // So the layout happens at the right place
 
-        // Figure out how much space the event title will take, and create a
-        // String fragment that will fit in the rectangle.  Use multiple lines,
-        // if available.
-        p.getTextWidths(text, mCharWidths);
-        String fragment = text;
-        float top = rf.top + mEventTextAscent + topMargin;
-        int start = 0;
+	StaticLayout layout=new StaticLayout( text, 0, text.length(), new TextPaint(p), (int)rf.width()-2,
+		Alignment.ALIGN_NORMAL, (float)1, (float)0.0, true, TextUtils.TruncateAt.END,
+	       	MAX_EVENT_TEXT_LEN );
+	
+	layout.draw( canvas );
 
-        // Leave one pixel extra space at the bottom
-        while (start < len && height >= (lineHeight + 1)) {
-            boolean lastLine = (height < 2 * lineHeight + 1);
-            // Skip leading spaces at the beginning of each line
-            do {
-                char c = text.charAt(start);
-                if (c != ' ') break;
-                start += 1;
-            } while (start < len);
-
-            float sum = 0;
-            int end = start;
-            for (int ii = start; ii < len; ii++) {
-                char c = text.charAt(ii);
-
-                // If we found the end of a word, then remember the ending
-                // position.
-                if (c == ' ') {
-                    end = ii;
-                }
-                sum += mCharWidths[ii];
-                // If adding this character would exceed the width and this
-                // isn't the last line, then break the line at the previous
-                // word.  If there was no previous word, then break this word.
-                if (sum > width) {
-                    if (end > start && !lastLine) {
-                        // There was a previous word on this line.
-                        fragment = text.substring(start, end);
-                        start = end;
-                        break;
-                    }
-
-                    // This is the only word and it is too long to fit on
-                    // the line (or this is the last line), so take as many
-                    // characters of this word as will fit.
-                    fragment = text.substring(start, ii);
-                    start = ii;
-                    break;
-                }
-            }
-
-            // If sum <= width, then we can fit the rest of the text on
-            // this line.
-            if (sum <= width) {
-                fragment = text.substring(start, len);
-                start = len;
-            }
-
-            canvas.drawText(fragment, rf.left + 1, top, p);
-
-            top += lineHeight;
-            height -= lineHeight;
-        }
+	canvas.restore();
     }
 
     private void updateEventDetails() {
