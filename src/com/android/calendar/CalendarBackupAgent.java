b14a64ead922f2fc29718c4a5f4c14d3ae181a64
garth <garth.bushell@gmail.com>
2010-10-06 23:24:37 +0100

    Getting used to git. Patch to backup settings using BackupAgentHelper
    Change-Id: Ie6bc2fce98ec991ef720d792f2cc61b250dcae37

--- src/com/android/calendar/CalendarBackupAgent.java
+++ src/com/android/calendar/CalendarBackupAgent.java
@@ -1,15 +1,13 @@
 package com.android.calendar;
 
 import android.app.backup.BackupAgentHelper;
-import android.app.backup.FileBackupHelper;
 import android.app.backup.SharedPreferencesBackupHelper;
 
 public class CalendarBackupAgent extends BackupAgentHelper
 {
     static final String SHARED_KEY = "shared_pref";
 
-    public void onCreate ()
-    {
+    public void onCreate () {
         addHelper(SHARED_KEY, new SharedPreferencesBackupHelper(this,
                                                                 CalendarPreferenceActivity.SHARED_PREFS_NAME));
     }
