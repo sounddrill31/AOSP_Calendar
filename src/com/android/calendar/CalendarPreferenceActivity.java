62587c384abe8b6f82b0cd574146568ffc51cfe0
garth <garth.bushell@gmail.com>
2010-10-16 17:25:07 +0100

    remove debug logging acedently commited
    Change-Id: Icb32422fbb7126a9c0f88560aa9daacf9121f297

--- src/com/android/calendar/CalendarPreferenceActivity.java
+++ src/com/android/calendar/CalendarPreferenceActivity.java
@@ -33,8 +33,6 @@ import android.util.Log;
 public class CalendarPreferenceActivity extends PreferenceActivity implements OnSharedPreferenceChangeListener {
     private static final String BUILD_VERSION = "build_version";
 
-    private static final String TAG = "CalendarPreferenceActivity";
-
     // The name of the shared preferences file. This name must be maintained for historical
     // reasons, as it's what PreferenceManager assigned the first time the file was created.
     static final String SHARED_PREFS_NAME = "com.android.calendar_preferences";
@@ -118,7 +116,6 @@ public class CalendarPreferenceActivity extends PreferenceActivity implements On
         if (key.equals(KEY_ALERTS_TYPE)) {
             updateChildPreferences();
         }
-	Log.v(TAG, "onSharedPreferenceChanged:" + this.getPackageName());
 	BackupManager.dataChanged(this.getPackageName());
     }
 
